0772a03230892755ee54ab6fed95bcfceb829b06
Tom <tomdesvignes031@gmail.com>
2023-06-12 11:12:46 +0200

    Initial Commit

---
+++ Cours C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cours C++
+//
+//  Created by Tom Desvignes on 12/06/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
